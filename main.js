5932bf920631600da38ea4bf01a1669bc2ead401
LorenzoMoiana <136532510+LorenzoMoiana@users.noreply.github.com>
2023-09-07 19:48:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 StudentArray = [];
 function Submit()
-{
+
+
     var displayStudentArray = [];
     for (var j = 1; j <= 4; j++)
     {
@@ -10,7 +11,7 @@ function Submit()
 
     }
 
-    
+    console.log(StudentArray)
 
     var lengthOfNameOfStudentsArray = StudentsArray.length;
     console.log(lengthOfNameOfStudentsArray);
@@ -28,4 +29,31 @@ document.getElementById("displayNameWithCommas").innerHTML = displayStudentArray
 var removeCommas = displayStudentArray.join(" ");
 console.log(removeCommas);
 document.getElementById("submitButton").style.display = "none";
+
+function sorting()
+{
+    StudentArray.sort();
+    console.log(StudentArray);
+
+    var displayStudentArraySorting = [];
+
+    var lenghtOfNameOfStudentArray = StudentArray.lenght;
+    console.log(lenghtOfNameOfStudentArray);
+
+    for (var k = 0; k < lenghtOfNameOfStudentArray; k++)
+    {
+        displayStudentArraySorting.push("<h4>NOME - " + StudentAray[k] + "</h4>")
+        ;
+        console.log(displayStudentArraySorting);
+
+    }
+
+    var removeCommas = displayStudentArraySorting.join(" ");
+
+    document.getElementById("displayNameWithoutCommas").innerHTML = removeCommas;
+   
+    function newupdate()
+    {
+        document.getEementById("displayNameWithoutCommas").innerHTML = "<h1>" + StudentArray +"</h1>";
+    }
 }
